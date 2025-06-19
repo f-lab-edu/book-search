4ee9fe9ad67ce01cc616120bc834a33bbeb4e900
yjkim <yjkimdj@gmail.com>
2025-06-19 15:19:37 +0900

    feat: Toolbar 설정

--- app/src/main/java/kr/yjkim/book_search/HomeFragment.kt
+++ app/src/main/java/kr/yjkim/book_search/HomeFragment.kt
@@ -1,13 +1,13 @@
 package kr.yjkim.book_search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kr.yjkim.book_search.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment: Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
