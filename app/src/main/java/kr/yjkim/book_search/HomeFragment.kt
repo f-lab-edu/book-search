4cce6752581fc6fbeb6b028dee3d82b9368ab474
yjkim <yjkimdj@gmail.com>
2025-07-01 17:16:16 +0900

    feat: 검색창 설정

--- app/src/main/java/kr/yjkim/book_search/HomeFragment.kt
+++ app/src/main/java/kr/yjkim/book_search/HomeFragment.kt
@@ -1,10 +1,14 @@
 package kr.yjkim.book_search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import kr.yjkim.book_search.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment() {
@@ -15,6 +19,21 @@ class HomeFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tlSearch.editText?.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                v.clearFocus()
+                // keyboard close
+                val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+                // navigate layout
+                findNavController().navigate(R.id.list)
+                true
+            } else false
+        }
     }
 
     override fun onDestroyView() {
