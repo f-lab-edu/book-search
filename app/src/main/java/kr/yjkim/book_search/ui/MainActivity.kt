6061ae452ee6d5d224b76b63750f9884207bf999
yjkim <yjkimdj@gmail.com>
2025-08-06 21:23:51 +0900

    feat: 현재 화면 id 에 따라 뒤로 버튼 클릭 설정

--- app/src/main/java/kr/yjkim/book_search/ui/MainActivity.kt
+++ app/src/main/java/kr/yjkim/book_search/ui/MainActivity.kt
@@ -2,11 +2,13 @@ package kr.yjkim.book_search.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kr.yjkim.book_search.R
 import kr.yjkim.book_search.data.MyRepository
 import kr.yjkim.book_search.databinding.ActivityMainBinding
@@ -42,6 +44,29 @@ class MainActivity: AppCompatActivity() {
                 R.id.info, R.id.list -> toolbar.visibility = View.VISIBLE
             }
         }
+
+        // back button dispatcher
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val currentDestination = navController.currentDestination?.id
+                when {
+                    currentDestination == R.id.home -> MaterialAlertDialogBuilder(this@MainActivity)
+                        .setMessage(getString(R.string.dialog_message_exit))
+                        .setPositiveButton(getString(R.string.dialog_btn_yes_exit)) { dialog, _ ->
+                            finish()
+                            dialog.dismiss()
+                        }
+                        .setNeutralButton(getString(R.string.dialog_btn_no)) { dialog, _ ->
+                            dialog.cancel()
+                        }
+                        .setCancelable(false)
+                        .show()
+
+                    else -> navController.navigateUp()
+                }
+            }
+        })
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
