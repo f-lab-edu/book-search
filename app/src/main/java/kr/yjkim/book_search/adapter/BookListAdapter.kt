c2899ccb7553dc867c612da0ed4ef11b5ea1e0dc
yjkim <yjkimdj@gmail.com>
2025-08-06 21:11:26 +0900

    feat: BookListAdapter 및 item layout 수정
    - 함수를 통해 book list 주입
    - 책 설명 최대 두 줄로 제한, 줄임표 설정
    - hint 속성 제거, tools 이용하여 개발할 때만 보이도록 설정

--- app/src/main/java/kr/yjkim/book_search/adapter/BookListAdapter.kt
+++ app/src/main/java/kr/yjkim/book_search/adapter/BookListAdapter.kt
@@ -3,18 +3,38 @@ package kr.yjkim.book_search.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kr.yjkim.book_search.data.schema.BookItem
 import kr.yjkim.book_search.databinding.ItemBookListBinding
 
-class BookListAdapter(private val bookList: List<String>): RecyclerView.Adapter<BookListViewHolder>() {
+class BookListAdapter: RecyclerView.Adapter<BookListViewHolder>() {
+
+    private val bookList: MutableList<BookItem> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookListViewHolder {
         val binding = ItemBookListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return BookListViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: BookListViewHolder, position: Int) {}
+    override fun onBindViewHolder(holder: BookListViewHolder, position: Int) {
+        val bookItem = bookList[position]
+        holder.bind(bookItem)
+    }
 
     override fun getItemCount(): Int = bookList.size
+
+    fun submitList(list: List<BookItem>) {
+        bookList.clear()
+        bookList.addAll(list)
+        notifyDataSetChanged()
+    }
 }
 
-class BookListViewHolder(binding: ItemBookListBinding): RecyclerView.ViewHolder(binding.root)
+class BookListViewHolder(private val binding: ItemBookListBinding): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: BookItem) {
+        binding.tvTitle.text = item.title
+        binding.tvStc.text = item.contents
+
+        // thumbnail: Glide...
+    }
+}
--- app/src/main/java/kr/yjkim/book_search/ui/ListFragment.kt
+++ app/src/main/java/kr/yjkim/book_search/ui/ListFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kr.yjkim.book_search.R
 import kr.yjkim.book_search.adapter.BookListAdapter
 import kr.yjkim.book_search.databinding.FragmentListBinding
@@ -17,9 +16,8 @@ class ListFragment: Fragment() {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: BookListAdapter
     private val vm: MyViewModel by activityViewModels()
+    private val adapter = BookListAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentListBinding.inflate(inflater, container, false)
@@ -28,6 +26,7 @@ class ListFragment: Fragment() {
         (requireActivity() as MainActivity).setToolbar(toolbarTitleText, true)
 
         vm.books.observe(viewLifecycleOwner) { books ->
+            adapter.submitList(books)
         }
         return binding.root
     }
@@ -35,11 +34,8 @@ class ListFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        recyclerView = binding.recyclerBook
+        val recyclerView = binding.recyclerBook
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        val bookList = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13") // demo
-        adapter = BookListAdapter(bookList)
         recyclerView.adapter = adapter
     }
 
