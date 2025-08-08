45a039cb091ffcfb26b685d12cd87464fdade906
yjkim <yjkimdj@gmail.com>
2025-08-08 19:56:30 +0900

    refactor: BuildConfig 클래스 명시

--- app/src/main/java/kr/yjkim/book_search/data/network/KakaoService.kt
+++ app/src/main/java/kr/yjkim/book_search/data/network/KakaoService.kt
@@ -1,6 +1,6 @@
 package kr.yjkim.book_search.data.network
 
-import kr.yjkim.book_search.BuildConfig.REST_API_KEY
+import kr.yjkim.book_search.BuildConfig
 import kr.yjkim.book_search.data.schema.BookResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 
 interface KakaoService {
 
-    @Headers("Authorization: KakaoAK $REST_API_KEY")
+    @Headers("Authorization: KakaoAK ${BuildConfig.REST_API_KEY}")
     @GET("v3/search/book")
     suspend fun getBookList(
         @Query("query") keyword: String,
