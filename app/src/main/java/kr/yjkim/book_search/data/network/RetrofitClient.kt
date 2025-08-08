12598a1bbf162e1dea589121a0c382fc88e4cd33
yjkim <yjkimdj@gmail.com>
2025-08-08 19:49:38 +0900

    refactor: moshi 객체 이동, MoshiBuilder 제거

--- app/src/main/java/kr/yjkim/book_search/data/network/RetrofitClient.kt
+++ app/src/main/java/kr/yjkim/book_search/data/network/RetrofitClient.kt
@@ -1,10 +1,15 @@
 package kr.yjkim.book_search.data.network
 
-import kr.yjkim.book_search.data.network.MoshiBuilder.moshi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
+
+    private val moshi: Moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
 
     val kakaoService: KakaoService = Retrofit.Builder()
         .baseUrl("https://dapi.kakao.com/")
