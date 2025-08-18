0960becefd1151a92d3e766335dd6fbdad5bec98
yjkim <yjkimdj@gmail.com>
2025-08-18 18:03:51 +0900

    refactor: 객체 타입 명시

--- app/src/main/java/kr/yjkim/book_search/ui/ListViewModel.kt
+++ app/src/main/java/kr/yjkim/book_search/ui/ListViewModel.kt
@@ -1,14 +1,16 @@
 package kr.yjkim.book_search.ui
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import kr.yjkim.book_search.data.BookSearchRepository
+import kr.yjkim.book_search.data.schema.BookItem
 
 class ListViewModel(repository: BookSearchRepository): ViewModel() {
 
-    val bookList = repository.bookList
+    val bookList: LiveData<List<BookItem>> = repository.bookList
 
     companion object {
         fun create(repo: BookSearchRepository): ViewModelProvider.Factory {
