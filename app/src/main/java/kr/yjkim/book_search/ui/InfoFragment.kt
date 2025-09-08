6da1d5fcdd3078f48047779501c22d4d33cd46bc
yjkim <yjkimdj@gmail.com>
2025-09-08 20:39:46 +0900

    feat: InfoFragment 에 책 상세 내용 표출
    - 전달 받은 position 에 해당하는 데이터를 LiveData 에 담고, 다시 그 LiveData 를 추적하여 UI에 표출

--- app/src/main/java/kr/yjkim/book_search/ui/InfoFragment.kt
+++ app/src/main/java/kr/yjkim/book_search/ui/InfoFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import coil3.load
 import kr.yjkim.book_search.data.BookSearchRepository
+import kr.yjkim.book_search.data.schema.BookItem
 import kr.yjkim.book_search.databinding.FragmentInfoBinding
 
 class InfoFragment: Fragment() {
@@ -17,10 +20,24 @@ class InfoFragment: Fragment() {
     private val vm: InfoViewModel by viewModels {
         InfoViewModel.create(BookSearchRepository)
     }
+    private val args: InfoFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentInfoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        vm.getBookItem(args.itemId)
+
+        vm.bookItem.observe(viewLifecycleOwner) { bookItem: BookItem ->
+            val toolbarTitleText = bookItem.title
+            (requireActivity() as MainActivity).setToolbar(toolbarTitleText, true)
+            binding.bookThumbnail.load(bookItem.thumbnail)
+            binding.bookSentence.text = bookItem.contents
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/kr/yjkim/book_search/ui/InfoViewModel.kt
+++ app/src/main/java/kr/yjkim/book_search/ui/InfoViewModel.kt
@@ -1,12 +1,23 @@
 package kr.yjkim.book_search.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import kr.yjkim.book_search.data.BookSearchRepository
+import kr.yjkim.book_search.data.schema.BookItem
 
 class InfoViewModel(val repository: BookSearchRepository): ViewModel() {
+
+    private val _bookItem: MutableLiveData<BookItem> = MutableLiveData()
+    val bookItem: LiveData<BookItem> get() = _bookItem
+
+    fun getBookItem(itemId: Int) {
+        val result = requireNotNull(repository.searchResult.value)
+        _bookItem.value = result.getOrThrow()[itemId]
+    }
 
     companion object {
         fun create(repo: BookSearchRepository): ViewModelProvider.Factory {
