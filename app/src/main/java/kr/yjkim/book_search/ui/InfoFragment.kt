d6b7e37d729679264b3073d5f2d65aa1ecc47407
yjkim <yjkimdj@gmail.com>
2025-09-11 15:53:58 +0900

    feat: InfoViewModel 불필요하여 삭제

--- app/src/main/java/kr/yjkim/book_search/ui/InfoFragment.kt
+++ app/src/main/java/kr/yjkim/book_search/ui/InfoFragment.kt
@@ -5,10 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import coil3.load
-import kr.yjkim.book_search.data.BookSearchRepository
 import kr.yjkim.book_search.data.network.RetrofitClient.moshi
 import kr.yjkim.book_search.data.schema.BookItem
 import kr.yjkim.book_search.databinding.FragmentInfoBinding
@@ -18,9 +16,6 @@ class InfoFragment: Fragment() {
     private var _binding: FragmentInfoBinding? = null
     private val binding get() = _binding!!
 
-    private val vm: InfoViewModel by viewModels {
-        InfoViewModel.create(BookSearchRepository)
-    }
     private val args: InfoFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -34,7 +29,7 @@ class InfoFragment: Fragment() {
         val moshiAdapter = moshi.adapter(BookItem::class.java)
         val bookItem = moshiAdapter.fromJson(args.bookItemJson)
 
-        vm.bookItem.observe(viewLifecycleOwner) { bookItem: BookItem ->
+        bookItem?.let {
             val toolbarTitleText = bookItem.title
             (requireActivity() as MainActivity).setToolbar(toolbarTitleText, true)
             binding.bookThumbnail.load(bookItem.thumbnail)
